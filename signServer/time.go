6afefb929b16d77e0f6357ae6aaab9ab0b9168ed
尚富荣 <82471454@qq.com>
2022-12-08 17:22:37 +0800

    feat: 打卡服务

---
+++ signServer/time.go
@@ -0,0 +1,18 @@
+package signServer
+
+import "time"
+
+// 获取1970年1月1日 距今多少天 - 19300
+func getTodayNum() int64 {
+	t := time.Now().Unix()
+	diff := float64(t)
+	d := int64(diff * 10 / (3600 * 24))
+
+	if d%10 > 0 {
+		d = d/10 + 1
+	} else {
+		d = d / 10
+	}
+	// 数字太多不利于存储, 因为会获取近7天的数据, 所以 尽量不要让减去的数据接近 1970-1-1 至今的天 靠近
+	return d - 19300
+}
